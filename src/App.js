a4c6b6959fc4de71a43c121b0c9f61f2f6ae7a64
mannyvh62 <155288697+mannyvh62@users.noreply.github.com>
2024-02-16 09:52:38 -0800

    create pages and routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
- 
+ import { BrowserRouter as Router, Routes,Route } from "react-router-dom";
+ import Home from "./pages/Home";
+ import Profile from "./pages/Profile";
+ import SignIn from "./pages/SignIn";
+ import SignUp from "./pages/SignUp";
+ import ForgotPassword from "./pages/ForgotPassword";
+ import Offers from "./pages/Offers";
 
 function App() {
   return (
     <>
-      <h1 className=''> Home</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home/>}/>   <Route path="/" element={<Home/>}/>
+          <Route path="/profile" element={<Profile/>}/>
+          <Route path="/sign-in" element={<SignIn/>}/>
+          <Route path="/sign-Up" element={<SignUp/>}/>
+          <Route path="/forgot-password" element={<ForgotPassword/>}/>
+          <Route path="/Offers" element={<Offers/>}/>
+        </Routes>
+      </Router>
     </>
   );
 }
